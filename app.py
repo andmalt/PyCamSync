aa6369cdcbd575aa0bc05f44b780c33f04b74d5c
andmalt <andrea.maltempi@gmail.com>
2023-10-13 17:18:47 +0200

    added ini to configure the app and added argparse

--- app.py
+++ app.py
@@ -1,7 +1,47 @@
 # from cam import record_video
+import argparse
+from configparser import RawConfigParser
+import os
+import sys
+import time
 from src.cam import record_video
 
-def main():
-    record_video()
-    
-main()
+def loadIni(filename):
+    if (os.path.isfile(filename)):
+        config = RawConfigParser()
+        config.read(filename)
+        return config
+    else:
+        print("ERROR: Configuration file not found")
+        sys.exit(1)
+
+
+def main(config:RawConfigParser):
+    record_video(config)
+    # while True:
+    #     time.sleep(120)
+    #     try:
+    #         record_video(config)
+    #     except Exception as e:
+    #         print("Exception webcam")
+    #         print(e)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='pycamsync',
+                                     description='This program runs the webcam to record and save the file in a directory set by you',
+                                     epilog='(C) 2023 Andrea Maltempi, All rights reserved.')
+
+    parser.add_argument('-i', '--ini', help='Path to INI file (default ./pycamsync.ini)',
+                        action='store', required=False, default='./pycamsync.ini')
+
+    print("\nPyCamSync Version 1.0")
+    print("--------------------------------------------------")
+    print("(C) 2023 Andrea Maltempi - All rights reserved.\n")
+
+    args = parser.parse_args()
+
+    # Lettura del file di configurazione
+    config = loadIni(args.ini)
+
+    main(config)
--- src/cam.py
+++ src/cam.py
@@ -1,7 +1,12 @@
+from configparser import RawConfigParser
+import os
 import cv2
 from datetime import datetime
 
-def record_video():
+def record_video(config:RawConfigParser):
+    folder = str(config["FOLDERS"]["DIR"])
+    
+    print("Waiting....")
     # Accendi la webcam
     webcam = cv2.VideoCapture(0)
 
@@ -15,10 +20,18 @@ def record_video():
     video_filename  =  f"video_{formatted_data}.avi"
     # print(video_filename)
     
+    directory_path = os.path.join(folder, "Compressed")
+    # create directory if it doesn't exist
+    try:
+        os.makedirs(directory_path)
+    except:
+        pass
+    
+    video_path = os.path.join(directory_path,video_filename)
     # Imposta il codec e crea un oggetto VideoWriter per salvare il video
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    out = cv2.VideoWriter(video_filename, fourcc, 30.0, (640, 480)) # HD 1280 - 720
-    print('ok')
+    out = cv2.VideoWriter(video_path, fourcc, 30.0, (640, 480)) # HD 1280 - 720
+    print('Video is recording')
     
     while True:
         # Leggi un frame dalla webcam
